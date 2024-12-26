7a1977a453568c4473f081569cd4bac883c03b92
paddy sherry <hello@paddysherry.com>
2024-12-27 00:37:33 +0100

    backend page to add API token

--- SG Widget/index.php
+++ SG Widget/index.php
@@ -18,8 +18,11 @@ function sg_widget_shortcode($atts) {
         return '<p>Error: No ID provided for sg_widget shortcode.</p>';
     }
 
-    // Construct the API URL
-    $api_url = "https://app.sgwidget.com/api/widget/{$id}";
+    // Retrieve the API token from the database
+    $api_token = get_option('sg_widget_api_token', '');
+
+    // Construct the API URL with the token
+    $api_url = "https://app.sgwidget.com/api/widget/{$id}?api_token={$api_token}";
 
     // Make the GET request
     $response = wp_remote_get($api_url);
@@ -38,3 +41,60 @@ function sg_widget_shortcode($atts) {
 
 // Register the shortcode
 add_shortcode('sg_widget', 'sg_widget_shortcode');
+
+// Add a menu item for the plugin settings page
+add_action('admin_menu', 'sg_widget_add_admin_menu');
+
+function sg_widget_add_admin_menu() {
+    add_options_page(
+        'SG Widget Settings',
+        'SG Widget',
+        'manage_options',
+        'sg-widget',
+        'sg_widget_options_page'
+    );
+}
+
+function sg_widget_options_page() {
+    ?>
+    <div class="wrap">
+        <h1>SG Widget Settings</h1>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('sg_widget_options');
+            do_settings_sections('sg-widget');
+            submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+// Register the settings
+add_action('admin_init', 'sg_widget_settings_init');
+
+function sg_widget_settings_init() {
+    register_setting('sg_widget_options', 'sg_widget_api_token');
+
+    add_settings_section(
+        'sg_widget_section',
+        __('API Settings', 'sg_widget'),
+        null,
+        'sg-widget'
+    );
+
+    add_settings_field(
+        'sg_widget_api_token',
+        __('API Token', 'sg_widget'),
+        'sg_widget_api_token_render',
+        'sg-widget',
+        'sg_widget_section'
+    );
+}
+
+function sg_widget_api_token_render() {
+    $api_token = get_option('sg_widget_api_token', '');
+    ?>
+    <input type='text' name='sg_widget_api_token' value='<?php echo esc_attr($api_token); ?>'>
+    <?php
+}
